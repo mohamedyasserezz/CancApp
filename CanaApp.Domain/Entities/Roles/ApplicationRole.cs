5b9faecede7f3197c15a8722b34de0931e3a98ef
Mohamed Yasser <mohamedyasserezz067@gmail.com>
2025-03-04 15:39:08 +0200

    Add ApplicationRole

---
+++ CanaApp.Domain/Entities/Roles/ApplicationRole.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace CanaApp.Domain.Entities.Roles
+{
+    class ApplicationRole : IdentityRole
+    {
+        public bool IsDefault { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
