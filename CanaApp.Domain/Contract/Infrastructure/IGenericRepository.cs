e5d02a162f12b4dc93b027bc92b5584ccaedc323
Mohamed Yasser <mohamedyasserezz067@gmail.com>
2025-03-04 19:50:17 +0200

    Adding GenericRepository

---
+++ CanaApp.Domain/Contract/Infrastructure/IGenericRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace CanaApp.Domain.Contract.Infrastructure
+{
+    public interface IGenricRepository<TEntity> where TEntity : class
+    {
+        public Task<IEnumerable<TEntity>> GetAllAsync(bool withTracking = false, CancellationToken cancellationToken = default);
+        public Task<TEntity?> GetByIdAsync(int id, CancellationToken cancellationToken = default);
+        public Task AddAsync(TEntity entity, CancellationToken cancellationToken = default);
+        public void Update(TEntity entity);
+        public void Delete(TEntity entity);
+
+    }
+}
---
+++ CanaApp.Persistance/GenericRepository/GenericRepository.cs
@@ -0,0 +1,39 @@
+﻿using CanaApp.Domain.Contract.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
+namespace CanaApp.Persistance.GenericRepository
+{
+    public class GenericRepository<TEntity>(ApplicationDbContext _dbContext) : IGenricRepository<TEntity>
+        where TEntity : class
+    {
+        #region Fields
+
+        #endregion
+        public async Task<IEnumerable<TEntity>> GetAllAsync(bool withTracking = false, CancellationToken cancellationToken = default)
+        {
+            return withTracking ?
+                await _dbContext.Set<TEntity>().ToListAsync(cancellationToken) :
+                await _dbContext.Set<TEntity>().AsNoTracking().ToListAsync(cancellationToken);
+        }
+
+        public async Task<TEntity?> GetByIdAsync(int id, CancellationToken cancellationToken = default)
+        {
+            return await _dbContext.Set<TEntity>().FindAsync(id, cancellationToken);
+        }
+
+        public async Task AddAsync(TEntity entity, CancellationToken cancellationToken = default)
+        {
+            await _dbContext.Set<TEntity>().AddAsync(entity, cancellationToken);
+        }
+
+        public void Update(TEntity entity)
+        {
+            _dbContext.Set<TEntity>().Update(entity);
+        }
+        public void Delete(TEntity entity)
+        {
+            _dbContext.Remove(entity);
+        }
+
+    }
+}
