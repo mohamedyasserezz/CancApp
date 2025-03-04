1a6c8f6619ec74907df95c9e423bc23e19682b62
Mohamed Yasser <mohamedyasserezz067@gmail.com>
2025-03-04 17:09:22 +0200

    Add Result Pattern

---
+++ CancApp.Shared/Abstractions/Error.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CancApp.Shared.Abstractions
+{
+    public record Error(string Code, string Description, int? StatusCode)
+    {
+        public static readonly Error None = new(string.Empty, string.Empty, null);
+    }
+}
---
+++ CancApp.Shared/Abstractions/Result.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CancApp.Shared.Abstractions
+{
+    public class Result
+    {
+        public Result(bool isSuccess, Error error)
+        {
+            if ((isSuccess && error != Error.None) || (!isSuccess && error == Error.None))
+                throw new InvalidOperationException();
+
+            IsSuccess = isSuccess;
+            Error = error;
+        }
+
+        public bool IsSuccess { get; }
+        public bool IsFailure => !IsSuccess;
+        public Error Error { get; } = default!;
+
+        public static Result Success() => new Result(true, Error.None);
+        public static Result<TValue> Success<TValue>(TValue value) => new(value, true, Error.None);
+        public static Result Failer(Error error) => new(false, error);
+        public static Result<TValue> Failer<TValue>(Error error) => new(default, false, error);
+    }
+    public class Result<TValue>(TValue? value, bool isSuccess, Error error) : Result(isSuccess, error)
+    {
+        private readonly TValue? _value = value;
+
+        public TValue Value => IsSuccess ?
+            _value! :
+            throw new InvalidOperationException("Faliure results cannot have value");
+    }
+}
