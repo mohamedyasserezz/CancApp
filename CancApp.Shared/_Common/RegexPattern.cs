b06ccc7769f0201c3c7029de0300693f0e4e3483
Mohamed Yasser <mohamedyasserezz067@gmail.com>
2025-03-06 14:37:35 +0200

    Add Regex Pattern

---
+++ CancApp.Shared/_Common/RegexPattern.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CancApp.Shared._Common
+{
+    public static class RegexPatterns
+    {
+        public const string Email = @"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$";
+        public const string Password = @"^(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9]).{8,}$";
+        public const string Name = @"^[a-zA-Z\s]+$";
+    }
+}
