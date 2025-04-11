4102c73ac32ac92fee3c7777d464b3ecb3ff4efe
Mohamed Yasser <mohamedyasserezz067@gmail.com>
2025-04-11 10:17:04 +0200

    but contract for specification pattern

---
+++ CanaApp.Application/Services/Community/ReactionService.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using CanaApp.Domain.Contract.Infrastructure;
+using CanaApp.Domain.Contract.Service.Community;
+using CancApp.Shared.Abstractions;
+using CancApp.Shared.Models.Community.Reactions;
+using Microsoft.Extensions.Logging;
+
+namespace CanaApp.Application.Services.Community
+{
+    class ReactionService(
+        IUnitOfWork unitOfWork,
+        IMapper mapper, 
+        ILogger<ReactionService> logger
+        ) : IReactionService
+    {
+        private readonly IUnitOfWork _unitOfWork = unitOfWork;
+        private readonly IMapper _mapper = mapper;
+        private readonly ILogger<ReactionService> _logger = logger;
+
+        public Task<Result<IEnumerable<ReactionResponse>>> GetReactionsAsync(int postId, int? commentId)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<Result> AddReactionAsync(ReactionRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public Task<Result> RemoveReactionAsync(ReactionRequest request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
