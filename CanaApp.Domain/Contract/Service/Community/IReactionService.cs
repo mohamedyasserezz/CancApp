09aa38b17c267fe14ac01027cdc4b683798db2e0
Mohamed Yasser <mohamedyasserezz067@gmail.com>
2025-04-11 09:07:15 +0200

    Add IReactionService

---
+++ CanaApp.Domain/Contract/Service/Community/IReactionService.cs
@@ -0,0 +1,13 @@
+﻿using CancApp.Shared.Abstractions;
+using CancApp.Shared.Models.Community.Reactions;
+
+namespace CanaApp.Domain.Contract.Service.Community
+{
+    public interface IReactionService
+    {
+        Task<Result> AddReactionAsync(ReactionRequest request);
+        Task<Result<IEnumerable<ReactionResponse>>> GetReactionsAsync(int postId, int? commentId);
+        Task<Result> RemoveReactionAsync(ReactionRequest request);
+
+    }
+}
