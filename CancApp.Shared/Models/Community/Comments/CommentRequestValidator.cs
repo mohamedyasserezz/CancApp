2147900184ed57711183a6d3a893e2446626f2ae
Mohamed Yasser <mohamedyasserezz067@gmail.com>
2025-04-12 22:24:58 +0200

    Add CommentRequestValidator

---
+++ CancApp.Shared/Models/Community/Comments/CommentRequestValidator.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+
+namespace CancApp.Shared.Models.Community.Comments
+{
+    public class CommentRequestValidator : AbstractValidator<CommentRequest>
+    {
+        public CommentRequestValidator()
+        {
+            RuleFor(x => x.Content)
+                .NotEmpty()
+                .WithMessage("Content cannot be empty.")
+                .MaximumLength(500)
+                .WithMessage("Content cannot exceed 500 characters.");
+
+            RuleFor(x => x.UserId)
+                .NotEmpty()
+                .WithMessage("UserId cannot be empty.");
+
+            RuleFor(x => x.PostId)
+                .NotEmpty()
+                .WithMessage("PostId cannot be empty.");
+
+        }
+    }
+   
+}
