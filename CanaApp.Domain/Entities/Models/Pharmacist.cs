4b64c29e23104ab2a8eb679dd3ce7cfbefb54605
Mohamed Yasser <mohamedyasserezz067@gmail.com>
2025-04-18 20:58:45 +0200

    Make some Attributes nullale

--- CanaApp.Domain/Entities/Models/Pharmacist.cs
+++ CanaApp.Domain/Entities/Models/Pharmacist.cs
@@ -4,8 +4,9 @@ namespace CanaApp.Domain.Entities.Models
 {
     public class Pharmacist
     {
-        public string ImageId { get; set; } = default!;
-        public string ImagePharmacyLicense { get; set; } = default!; public bool IsConfirmedByAdmin { get; set; }
+        public string? ImageId { get; set; } 
+        public string? ImagePharmacyLicense { get; set; }
+        public bool IsConfirmedByAdmin { get; set; }
         public ApplicationUser ApplicationUser { get; set; } = default!;
         public string UserId { get; set; } = default!;
         public bool IsDisabled { get; set; }
--- CanaApp.Domain/Entities/Models/Psychiatrist.cs
+++ CanaApp.Domain/Entities/Models/Psychiatrist.cs
@@ -4,8 +4,8 @@ namespace CanaApp.Domain.Entities.Models
 {
     public class Psychiatrist
     {
-        public string MedicalSyndicatePhoto { get; set; } = default!;
-        public string ImageId { get; set; } = default!;
+        public string? MedicalSyndicatePhoto { get; set; } 
+        public string? ImageId { get; set; }
         public ApplicationUser ApplicationUser { get; set; } = default!;
         public string UserId { get; set; } = default!;
         public bool IsDisabled { get; set; }
