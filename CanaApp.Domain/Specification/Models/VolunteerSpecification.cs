33385a0ed54b3d2b43bea5bf72d67e3d8d4e10a2
Mohamed Yasser <mohamedyasserezz067@gmail.com>
2025-04-18 18:28:43 +0200

    Add Volunteer Specification

---
+++ CanaApp.Domain/Specification/Models/VolunteerSpecification.cs
@@ -0,0 +1,18 @@
+﻿using CanaApp.Domain.Entities.Models;
+using System.Linq.Expressions;
+
+namespace CanaApp.Domain.Specification.Models
+{
+    public class VolunteerSpecification : Specification<Volunteer, int>
+    {
+        public VolunteerSpecification(Expression<Func<Volunteer, bool>>? expression) : base(expression)
+        {
+            AddIncludes();
+        }
+        private protected override void AddIncludes()
+        {
+            Includes.Add(x => x.ApplicationUser);
+        }
+    }
+
+}
