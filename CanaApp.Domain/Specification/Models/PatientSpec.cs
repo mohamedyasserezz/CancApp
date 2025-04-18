cb9aef6799a3de94542cdddf08ab1636ecf410c9
Mohamed Yasser <mohamedyasserezz067@gmail.com>
2025-04-18 18:24:34 +0200

    Add Patient Specification

---
+++ CanaApp.Domain/Specification/Models/PatientSpec.cs
@@ -0,0 +1,17 @@
+﻿using CanaApp.Domain.Entities.Models;
+using System.Linq.Expressions;
+
+namespace CanaApp.Domain.Specification.Models
+{
+    public class PatientSpec : Specification<Patient, int>
+    {
+        public PatientSpec(Expression<Func<Patient, bool>>? expression) : base(expression)
+        {
+
+        }
+        private protected override void AddIncludes()
+        {
+            Includes.Add(x => x.ApplicationUser);
+        }
+    }
+}
