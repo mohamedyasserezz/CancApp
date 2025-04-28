0b537f2445bd13f2ec2d0f1259cd3f19c0391d18
Mohamed Yasser <mohamedyasserezz067@gmail.com>
2025-04-28 16:41:12 +0300

    Add Request Filter

--- CancApp.Shared/_Common/Consts/RegexPattern.cs
+++ CancApp.Shared/_Common/Consts/RegexPattern.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CancApp.Shared.Common
+namespace CancApp.Shared._Common.Consts
 {
     public static class RegexPatterns
     {
---
+++ CancApp.Shared/_Common/Consts/RequestFilter.cs
@@ -0,0 +1,11 @@
+﻿namespace CancApp.Shared._Common.Consts
+{
+    public class RequestFilters
+    {
+        public int PageNumber { get; init; } = 1;
+        public int PageSize { get; init; } = 10;
+        public string? SearchValue { get; set; }
+        public string? SortColumn { get; set; }
+        public string? SortDirection { get; set; } = "ASC";
+    }
+}
