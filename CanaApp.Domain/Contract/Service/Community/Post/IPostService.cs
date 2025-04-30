5878ea72b5ec4dfa781c466e0664bdaed24339ba
Mohamed Yasser <mohamedyasserezz067@gmail.com>
2025-05-01 02:45:50 +0300

    Add IpostService

---
+++ CanaApp.Domain/Contract/Service/Community/Post/IPostService.cs
@@ -0,0 +1,18 @@
+﻿using CancApp.Shared._Common.Consts;
+using CancApp.Shared.Abstractions;
+using CancApp.Shared.Models.Community.Post;
+
+namespace CanaApp.Domain.Contract.Service.Community.Post
+{
+    public interface IPostService
+    {
+        Task<Result<PostResponse>> GetPostAsync(int postId, CancellationToken cancellationToken = default);
+        Task<Result<IEnumerable<PaginatedList<PostResponse>>>> GetPostsAsync(RequestFilters requestFilters, CancellationToken cancellationToken = default);
+        Task<Result> CreatePostAsync(PostRequest postRequest, CancellationToken cancellationToken = default);
+
+        Task<Result> UpdatePostAsync(UpdatePostRequest postRequest, CancellationToken cancellationToken = default);
+
+        Task<Result> DeletePostAsync(int postId, CancellationToken cancellationToken = default);
+
+    }
+}
