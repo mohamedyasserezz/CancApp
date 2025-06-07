f7e2902a3f04af659310e17ededcf7e648b3c7c0
Mohamed Yasser <mohamedyasserezz067@gmail.com>
2025-06-07 18:46:35 +0300

    Add Dashboard Models

---
+++ CancApp.Shared/Models/Dashboard/DashboardRequestValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CancApp.Shared.Models.Dashboard
+{
+    public class DashboardRequestValidator : AbstractValidator<DashboardRequest>
+    {
+        public DashboardRequestValidator()
+        {
+            RuleFor(x => x.id)
+                .NotEmpty()
+                .WithMessage("id can't be empty");
+        }
+    }
+}
---
+++ CancApp.Shared/Models/Dashboard/NumberOfUsersResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace CancApp.Shared.Models.Dashboard
+{
+    public record NumberOfUsersResponse(
+        int NumberOfUsers,
+        int NumberOfDoctors,
+        int NumberOfPatients,
+        int NumberOfPharmacist,
+        int NumberOfVolunteers,
+        int NumberOfPsychiatrist
+        );
+}
