385d879d869dcaace46ed28b27229c663c305b68
Mohamed Yasser <mohamedyasserezz067@gmail.com>
2025-06-07 18:46:58 +0300

    add dashboard contract

---
+++ CanaApp.Domain/Contract/Service/Dashboard/IDashboardServices.cs
@@ -0,0 +1,25 @@
+﻿using CancApp.Shared.Abstractions;
+using CancApp.Shared.Models.Authentication;
+using CancApp.Shared.Models.Community.Comments;
+using CancApp.Shared.Models.Dashboard;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CanaApp.Domain.Contract.Service.Dashboard
+{
+    public  interface IDashboardServices
+    {
+        Task<Result<AuthResponse>> Login(string email, string password, CancellationToken cancellationToken = default);
+
+        Task<Result<IEnumerable<CommentResponse>>> GetReportedCommentsAsync();
+
+        Task<Result> AddWarningToUserAsync(string id);
+        Task<Result> DisableUserAsync(string userId);
+
+        Task<Result> EnableUserAsync(string userId);
+        Task<Result<NumberOfUsersResponse>> GetUsersChart();
+    }
+}
