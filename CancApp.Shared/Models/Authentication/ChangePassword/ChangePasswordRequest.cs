95db69ae8739081400a468c3417df6920bdda738
Mohamed Yasser <mohamedyasserezz067@gmail.com>
2025-06-09 12:56:06 +0300

    Add ChangePassword Models

---
+++ CancApp.Shared/Models/Authentication/ChangePassword/ChangePasswordRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CancApp.Shared.Models.Authentication.ChangePassword
+{
+    public record ChangePasswordRequest(
+        string OldPassword,
+        string NewPassword
+        );
+}
---
+++ CancApp.Shared/Models/Authentication/ChangePassword/ChangePasswordRequestValidator.cs
@@ -0,0 +1,28 @@
+﻿using CancApp.Shared._Common.Consts;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CancApp.Shared.Models.Authentication.ChangePassword
+{
+    public class ChangePasswordRequestValidator : AbstractValidator<ChangePasswordRequest>
+    {
+        public ChangePasswordRequestValidator()
+        {
+            RuleFor(x => x.OldPassword)
+                .NotEmpty()
+                .WithMessage("Old password is required.")
+                .MinimumLength(6)
+                .WithMessage("Old password must be at least 6 characters long.");
+
+            RuleFor(x => x.NewPassword)
+                .NotEmpty()
+                .WithMessage("Plz Add a {PropertyName}")
+                .Matches(RegexPatterns.Password)
+                .WithMessage("Password should be atleast 8 digits and contains LowerCase, UpperCase, NonAlphanumeric");
+        }
+    }
+}
