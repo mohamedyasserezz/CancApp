a8640ca064a91d2819065265936bed4f0bf20fd6
Mohamed Yasser <mohamedyasserezz067@gmail.com>
2025-06-13 00:24:51 +0300

    Add Record Errrors and consts

---
+++ CancApp.Shared/_Common/Consts/Records.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CancApp.Shared._Common.Consts
+{
+    public static class Records
+    {
+        public const string Document = "Document";
+        public const string Scan = "Scan";
+        public const string LabResult = "LabResult";
+        public const string Prescription = "Prescription";
+    }
+}
---
+++ CancApp.Shared/_Common/Errors/RecordErrors.cs
@@ -0,0 +1,11 @@
+﻿using CancApp.Shared.Abstractions;
+using Microsoft.AspNetCore.Http;
+
+namespace CancApp.Shared._Common.Errors
+{
+    public static class RecordErrors
+    {
+        public static readonly Error RecordNotFound =
+            new("Record.RecordNotFound", "Record is not found", StatusCodes.Status404NotFound);
+    }
+}
