524ef4bd9f780cb67999c690d1cbaf447bedb00c
Mohamed Yasser <mohamedyasserezz067@gmail.com>
2025-06-13 00:24:10 +0300

    Add Record Contracts

---
+++ CanaApp.Domain/Contract/Service/Records/IRecordServices.cs
@@ -0,0 +1,13 @@
+﻿using CancApp.Shared.Abstractions;
+using CancApp.Shared.Models.Record;
+
+namespace CanaApp.Domain.Contract.Service.Record
+{
+    public interface IRecordServices
+    {
+        Task<Result<IEnumerable<RecordResponse>>> GetAllRecordsAsync(string userId);
+        Task<Result> CreateRecordAsync(RecordRequest request);
+        Task<Result> UpdateRecordAsync(UpdateRecordRequest request);
+        Task<Result> DeleteRecordAsync(int Id);
+    }
+}
