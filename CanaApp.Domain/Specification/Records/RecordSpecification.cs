18bc578759aa02e1ecd75f7d6ebac5b62dc072cd
Mohamed Yasser <mohamedyasserezz067@gmail.com>
2025-06-12 21:52:34 +0300

    Add RecordSpecification

---
+++ CanaApp.Domain/Specification/Records/RecordSpecification.cs
@@ -0,0 +1,18 @@
+﻿using CanaApp.Domain.Entities.Records;
+using System.Linq.Expressions;
+
+namespace CanaApp.Domain.Specification.Records
+{
+    public class RecordSpecification : Specification<Record, int>
+    {
+        public RecordSpecification(Expression<Func<Record, bool>>? expression) : base(expression)
+        {
+            AddIncludes();
+        }
+        private protected override void AddIncludes()
+        {
+            Includes.Add(r => r.User);
+            base.AddIncludes();
+        }
+    }
+}
