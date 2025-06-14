b4bd5e45b2fabc5e859f6cadfaf7bb5ed059f366
Mohamed Yasser <mohamedyasserezz067@gmail.com>
2025-06-14 16:47:32 +0300

    Add Get Users Models

---
+++ CancApp.Shared/Models/User/EditProfile/GetAllUsersRequestValidator.cs
@@ -0,0 +1,24 @@
+﻿using CancApp.Shared.Common.Consts;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CancApp.Shared.Models.User.EditProfile
+{
+    public class GetAllUsersRequestValidator : AbstractValidator<GetAllUsersRequest>
+    {
+        public GetAllUsersRequestValidator()
+        {
+            RuleFor(request => request.UserType)
+                .NotEmpty()
+                .WithMessage("User type is required.")
+                .Must(x => x is Users.Patient or Users.Doctor or Users.Volunteer or Users.Psychiatrist or Users.Pharmacist)
+                .WithMessage("Invalid user type specified.");
+        }
+
+
+    }
+}
---
+++ CancApp.Shared/Models/User/EditProfile/UserResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CancApp.Shared.Models.User.EditProfile
+{
+    public record UserResponse(
+        string Id,
+        string Name,
+        string Image,
+        string Address,
+        string UserType
+        );
+}
