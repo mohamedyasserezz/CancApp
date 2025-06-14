d289df8e4923ca5aafdb27cc026ed41af50f368d
Mohamed Yasser <mohamedyasserezz067@gmail.com>
2025-06-14 16:09:14 +0300

    Add PharmacistResponse

--- CancApp.Shared/Models/User/Pharmacy/PharmacistResponse.cs
+++ CancApp.Shared/Models/User/Pharmacy/PharmacistResponse.cs
@@ -9,6 +9,7 @@ namespace CancApp.Shared.Models.User.Pharmacy
     public record PharmacistResponse(
         string Name,
         string Image,
+        string Address,
         bool IsDeliveryEnabled,
         float Latitude,
         float Longitude,
