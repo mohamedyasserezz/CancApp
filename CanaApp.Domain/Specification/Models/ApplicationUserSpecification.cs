4fc8482e6ec04c96d3423588f0f14dc56859110c
Mohamed Yasser <mohamedyasserezz067@gmail.com>
2025-06-21 22:38:18 +0300

    Add ApplicationUserSpecification

---
+++ CanaApp.Domain/Specification/Models/ApplicationUserSpecification.cs
@@ -0,0 +1,19 @@
+﻿using CanaApp.Domain.Entities.Models;
+using System.Linq.Expressions;
+
+namespace CanaApp.Domain.Specification.Models
+{
+    public class ApplicationUserSpecification : Specification<ApplicationUser, string>
+    {
+
+        public ApplicationUserSpecification(Expression<Func<ApplicationUser, bool>>? expression) : base(expression)
+        {
+            AddIncludes();
+        }
+        private protected override void AddIncludes()
+        {
+            Includes.Add(u => u.FcmTokens);
+            base.AddIncludes();
+        }
+    }
+}
