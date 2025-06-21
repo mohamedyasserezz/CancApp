b10883da4076695197d87c4ac5b86b283082f412
Mohamed Yasser <mohamedyasserezz067@gmail.com>
2025-06-21 22:17:16 +0300

    add some Migration

---
+++ CanaApp.Persistance/Data/Migrations/20250615121115_AddFCMTokens.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CanaApp.Persistance.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFCMTokens : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "FcmTokens",
+                columns: table => new
+                {
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Token = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FcmTokens", x => new { x.UserId, x.Id });
+                    table.ForeignKey(
+                        name: "FK_FcmTokens_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: "18f2bb15-c5c3-4161-b3d5-ad8eab609da5",
+                column: "PasswordHash",
+                value: "AQAAAAIAAYagAAAAEGqR6OQ2cuLNIrGDgx2O0ztD08hr4uQr4P+O+eGp5pJ8uLi4P5onSe0SOuhAYP4JqA==");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: "6dc6528a-b280-4770-9eae-82671ee81ef7",
+                column: "PasswordHash",
+                value: "AQAAAAIAAYagAAAAEJIejaKR5/K1FkC33XcEeSrqbM9huWSGWYyJu7Mz7vzv1FMlVSkdgXRmxheTle2MJQ==");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: "8dbabc2f-4dd1-4ef6-acb4-7feb3fe5ed13",
+                column: "PasswordHash",
+                value: "AQAAAAIAAYagAAAAENGITEBMVsht5DctMR5D/RYwg0VpU6QkkpPk5SBAQty4eWx0k48hHWS8b3Ba5a96PA==");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FcmTokens");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: "18f2bb15-c5c3-4161-b3d5-ad8eab609da5",
+                column: "PasswordHash",
+                value: "AQAAAAIAAYagAAAAEClqc6hfzmjh4+F97EjcRYkFurGVyRhFaYP3C3nc3SzfgaQcKGhso6nO3vxhYDoW2Q==");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: "6dc6528a-b280-4770-9eae-82671ee81ef7",
+                column: "PasswordHash",
+                value: "AQAAAAIAAYagAAAAEMdKnoqDrb5+TjxYuaJ37xEtgRWR1HSXZezJiiBjgg8lDNS9K6xPtDWqVDr/PzwcYw==");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: "8dbabc2f-4dd1-4ef6-acb4-7feb3fe5ed13",
+                column: "PasswordHash",
+                value: "AQAAAAIAAYagAAAAEDit26wpgs2yVwbOw8QWwV5HibCQumofMM+PgIizjrL9nsz/lkWvflyDHcy4gMP32A==");
+        }
+    }
+}
